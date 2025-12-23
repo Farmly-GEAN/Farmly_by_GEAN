a26b0a54ffef7506b53955c8ad998dcf2a424976
NISHOK R R <56576788+nishok27@users.noreply.github.com>
2025-12-23 14:16:46 +0100

    Updating some changes

--- app/Views/Buyer/Buyer_Login.php
+++ app/Views/Buyer/Buyer_Login.php
@@ -61,37 +61,5 @@
     <?php include 'Footer.php'; ?>
   </body>
 
-  <script>
-    document
-      .getElementById("loginForm")
-      .addEventListener("submit", function (e) {
-        e.preventDefault();
-
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-
-        fetch("../Controllers/login.php", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-        })
-          .then((res) => res.text()) // TEMP: see raw response
-          .then((text) => {
-            console.log("RAW RESPONSE:", text);
-            const data = JSON.parse(text);
-
-            if (data.success) {
-              window.location.href = "HomePage.php";
-            } else {
-              document.getElementById("message").innerText = data.message;
-            }
-          })
-          .catch((error) => console.error("ERROR:", error));
-      });
-  </script>
+ 
 </html>
