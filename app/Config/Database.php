0106740739790ee4acd38f6644efa826afb2925b
NISHOK R R <56576788+nishok27@users.noreply.github.com>
2026-01-20 00:30:16 +0100

    Use DATABASE_URL for connection

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -8,32 +8,42 @@ class Database {
 
     public function getConnection() {
         $this->conn = null;
+        $port = "5432"; 
 
-        if (getenv('RAILWAY_ENVIRONMENT') || getenv('PGHOST') || getenv('POSTGRES_HOST')) {
+        e
+        if (getenv('DATABASE_URL')) {
+            $url = parse_url(getenv('DATABASE_URL'));
             
-            $this->host = getenv('PGHOST') ?: getenv('POSTGRES_HOST');
+            $this->host     = $url['host'];
+            $this->username = $url['user'];
+            $this->password = $url['pass'];
+            $this->db_name  = ltrim($url['path'], '/'); 
+            $port           = $url['port'];
             
-            $this->db_name = getenv('PGDATABASE') ?: getenv('POSTGRES_DB');
-            
+        } 
+        
+        elseif (getenv('PGHOST') || getenv('POSTGRES_HOST')) {
+            $this->host     = getenv('PGHOST') ?: getenv('POSTGRES_HOST');
+            $this->db_name  = getenv('PGDATABASE') ?: getenv('POSTGRES_DB');
             $this->username = getenv('PGUSER') ?: getenv('POSTGRES_USER');
-            
             $this->password = getenv('PGPASSWORD') ?: getenv('POSTGRES_PASSWORD');
-            
-            $port = getenv('PGPORT') ?: getenv('POSTGRES_PORT') ?: "5432";
-            
-        } else {
-            $port = "5432";
+            $port           = getenv('PGPORT') ?: getenv('POSTGRES_PORT') ?: "5432";
+        }
+        
+        
+        if (empty($this->host) && getenv('RAILWAY_ENVIRONMENT')) {
+             die("<strong>Configuration Error:</strong> Could not find Database Host. Please check Railway Variables.");
         }
 
         try {
-        
+            
             $dsn = "pgsql:host=" . $this->host . ";port=" . $port . ";dbname=" . $this->db_name;
             
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
         } catch(PDOException $exception) {
-            die("<strong>Database Connection Error:</strong> " . $exception->getMessage());
+            echo "Connection error: " . $exception->getMessage();
         }
 
         return $this->conn;
